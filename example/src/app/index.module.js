ad38300e9681eab56bbde10152f381f4f459ead2
Pavel Belugin <pashaigood@gmail.com>
2015-10-19 00:29:35 +0300

    Added example folder

---
+++ example/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('example', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
