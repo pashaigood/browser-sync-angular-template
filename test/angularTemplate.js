c15956c3ef520396dc16c916b44e0b3c664d369c
Pavel Belugin <pashaigood@gmail.com>
2015-10-24 03:03:30 +0300

    Added response to client
    * All templates have hash table 'template name' => 'selector id'.
    * Bs send template, template url and selector.
    * Unit-tests.

--- test/angularTemplate.js
+++ test/angularTemplate.js
@@ -1,8 +1,11 @@
 /* global it */
 'use strict';
 
+var fs = require('fs');
 var assert = require('assert');
 var angularTemplate = require('../lib/angularTemplate');
+var fileChanger = require('../lib/fileChanger');
+var path = require('path');
 var options = {
   templates: 'app/**/*.html',
   indexJs: 'index.modules.js'
@@ -48,5 +51,36 @@ describe('Angular template', function () {
 
   it('should catch index file by option "indexJs".', function() {
     assert(angularTemplate.isMatch('/app/index.modules.js', options.indexJs));
+  });
+
+  it('should create correct response for client.', function () {
+    fileChanger.id = 3;
+
+    var url = '/app/components/navbar/navbar.html',
+        templatePath = 'example/src' + url,
+        navbarTemplate = fs.readFileSync(templatePath, 'utf8'),
+        lastId = fileChanger.id,
+        absolutePath = path.normalize(__dirname + '/../' + templatePath),
+        markedTemplate;
+
+
+    assert(fs.existsSync(absolutePath));
+    // Для начала, запросим шаблон, чтобы создался хешь и пометки.
+    markedTemplate = angularTemplate.middleware({
+      url: url
+    })(navbarTemplate);
+    assert.equal(angularTemplate.templateHash[url], lastId, 'Template hash should be equal with last fileChanger id.');
+    // Потом, запросим ответ at на этот шаблон.
+    var res = angularTemplate.createResponse({
+      path: absolutePath
+    });
+
+    assert.deepEqual(res, {
+      selector: '[igat="%ID%"]'.replace('%ID%', lastId),
+      template: markedTemplate,
+      templateUrl: url
+    });
+
+    fileChanger.id = 0;
   });
 });
--- test/fileChanger.js
+++ test/fileChanger.js
@@ -6,33 +6,34 @@ var fs = require('fs');
 
 describe('File changer', function () {
   it('should append run injector.', function() {
+    var moduleName = 'example';
     var fileContent = fs.readFileSync('example/src/app/index.module.js', 'utf8');
-
-    var injectorContent = fs.readFileSync('client/runInjector.js');
-    assert(fileContent.length > 0, 'Content length should be more then 0');
-
-    var newContent = fileChanger.runInjector(fileContent, 'example');
-    assert(newContent.length > 0, 'New Content length should be more then 0');
-
-    assert(fileContent+injectorContent, newContent, 'Mock content and runtime content should be equal');
 
     assert.throws(fileChanger.runInjector);
 
     assert.throws(function() {
       fileChanger.runInjector(fileContent);
     });
+
+    var injectorContent = fs.readFileSync('client/runInjector.js', 'utf8');
+    var newContent = fileChanger.runInjector(fileContent, moduleName);
+
+    assert.equal(
+      fileContent + injectorContent.replace('%MODULE_NAME%', moduleName),
+      newContent,
+      'Mock content and runtime content should be equal'
+    );
   });
 
   it('should add template id.', function() {
     var mock = fs.readFileSync('test/mocks/navbar.html', 'utf8'),
         navbarTemplate = fs.readFileSync('example/src/app/components/navbar/navbar.html', 'utf8'),
         markedTemplate = fileChanger.markInjector(navbarTemplate);
+    assert.throws(fileChanger.markInjector);
 
-    console.log(markedTemplate);
-    assert(markedTemplate == mock.replace('%ID%', '1'), 'Templates should match.');
+    assert(markedTemplate == mock.replace('%ID%', '0'), 'Templates should match.');
     assert(markedTemplate != navbarTemplate);
 
-    assert(fileChanger.markInjector(navbarTemplate) == mock.replace('%ID%', '2'), 'Templates should match');
-    assert.throws(fileChanger.markInjector);
+    assert(fileChanger.markInjector(navbarTemplate) == mock.replace('%ID%', '1'), 'Templates should match.');
   });
 });
